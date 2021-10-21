b17bd414b59a8a74ce378e777cfec46cf16d30a3
Adrián Cardozo <adrian.cardozo1@alu.unq.edu.ar>
2021-10-21 11:44:34 -0300

    Se modificó el HUD

--- Entities/HUD/HUD.gd
+++ Entities/HUD/HUD.gd
@@ -1,15 +1,8 @@
-extends Control
+extends CanvasLayer
 
 
 func _ready():
-	var main_menu_button=preload("res://MainButtons/MainButton.tscn")	
-	var main_buttons=["Start game" , "How to Play"]
-	
-	for i in range(main_buttons.size()):
-		var instance= main_menu_button.instance()
-		instance.text=main_buttons[i]
-		$CenterContainer/PanelContainer/VBoxContainer/Buttons.add_child(instance)
-	
-	$CenterContainer/PanelContainer/VBoxContainer/Card.initialize($CenterContainer/PanelContainer/VBoxContainer/Player)
-	$CenterContainer/PanelContainer/VBoxContainer/Player.initialize($StatBarsManager)
-	$CenterContainer/PanelContainer/VBoxContainer/Card.emit_signal("update_values", "Hola mundo", "Aceptar", "Rechazar", {"Salud": -50}, {"Salud": -10})
+	pass
+	#$CenterContainer/PanelContainer/VBoxContainer/Card.initialize($CenterContainer/PanelContainer/VBoxContainer/Player)
+	#$CenterContainer/PanelContainer/VBoxContainer/Player.initialize($StatBarsManager)
+	#$CenterContainer/PanelContainer/VBoxContainer/Card.emit_signal("update_values", "Hola mundo", "Aceptar", "Rechazar", {"Salud": -50}, {"Salud": -10})
