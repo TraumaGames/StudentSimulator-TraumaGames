13a50b39cb3816eb8ba2b790cf79a9b2d07f8484
Adrián Cardozo <adrian.cardozo1@alu.unq.edu.ar>
2021-10-22 03:23:31 -0300

    Se agregó una historia al juego

--- Entities/StatBarsManager/StatBarsManager.gd
+++ Entities/StatBarsManager/StatBarsManager.gd
@@ -1,7 +1,10 @@
 extends Control
 class_name StatBarsManager
 
-onready var statBars = {"Salud": $HealthBar}
+onready var statBars = {
+	"Salud": $HealthBar,
+	"Ejercicios": $ExercisesBar
+	}
 
 func update_stats(stats):
 	for stat in stats.keys():
