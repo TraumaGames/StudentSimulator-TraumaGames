2929026b2cfffae306a5f53b7e845e6d9346abfc
Adrián Cardozo <adrian.cardozo1@alu.unq.edu.ar>
2021-11-11 12:45:36 -0300

    Se solucionó problema con texto de botones en card

--- Entities/Card/CardInformation/CardInformation.gd
+++ Entities/Card/CardInformation/CardInformation.gd
@@ -2,8 +2,6 @@ extends Node
 class_name CardInformation
 
 var parent
-#var next_card_accept: CardInformation
-#var next_card_cancel: CardInformation
 export var text: String
 export var textAccept: String
 export var textCancel: String
@@ -14,15 +12,6 @@ export var next_card_index_cancel: int = 0
 
 func set_parent(parent):
 	self.parent = parent
-
-func initialize(text, textAccept, textCancel, statsAccept, statsCancel):#, card_accept: CardInformation=null, card_cancel: CardInformation=null):
-	self.text = text
-	self.textAccept = textAccept
-	self.textCancel = textCancel
-	self.stats_accept = statsAccept
-	self.stats_cancel = statsCancel
-#	next_card_accept = card_accept
-#	next_card_cancel = card_cancel
 
 func next_card_accept():
 	return parent.cards[next_card_index_accept] if(next_card_index_accept >= 0) else null
