df1ff9b91b7aaa3bacd09fa6df32c1ddee8425e4
Lucas Morón <lucasmoron0512@gmail.com>
2021-12-02 22:16:16 -0300

    metodos show y hide para crosses

--- Entities/Calendar/Calendar.gd
+++ Entities/Calendar/Calendar.gd
@@ -1,2 +1,13 @@
 extends TextureButton
 
+func show_cross_up_to(dayNumber:int):
+	var number = dayNumber
+	var crosses = $Crosses.get_children().slice(0, number-1)
+	for c in crosses:
+		c.show()
+		
+
+func reset_calendar():
+	var crosses = $Crosses.get_children()
+	for c in crosses:
+		c.hide()
