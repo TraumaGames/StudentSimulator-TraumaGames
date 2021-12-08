8b51c1262a7643ee5868fbd12429d7037d0c7533
DiegoMoronha <diegomoronha@gmail.com>
2021-12-08 18:33:15 -0300

    se agregan sonidos de books y botellas

--- Entities/Books/Books.gd
+++ Entities/Books/Books.gd
@@ -7,17 +7,22 @@ export (Texture) var books2: Texture
 export (Texture) var books3: Texture
 export (Texture) var books4: Texture
 
+onready var book_sound =$AudioStreamPlayer
 onready var sprite: Sprite = $Sprite
 
 
 func update_stats(stats):
 	if stats[StatManager.Stats.BOOKS] > 0 && stats[StatManager.Stats.BOOKS] <= 25:
 		sprite.texture = books1
+		book_sound.play()
 	elif stats[StatManager.Stats.BOOKS] > 25 && stats[StatManager.Stats.BOOKS] <= 50:
 		sprite.texture = books2
+		book_sound.play()
 	elif stats[StatManager.Stats.BOOKS] > 50 && stats[StatManager.Stats.BOOKS] <= 75:
 		sprite.texture = books3
+		book_sound.play()
 	elif stats[StatManager.Stats.BOOKS] > 75:
 		sprite.texture = books4
+		book_sound.play()
 	else:
 		sprite.texture = null
--- Entities/Bottles/Bottles.gd
+++ Entities/Bottles/Bottles.gd
@@ -5,18 +5,22 @@ export (Texture) var bottles1: Texture
 export (Texture) var bottles2: Texture
 export (Texture) var bottles3: Texture
 export (Texture) var bottles4: Texture
-
+onready var bottle_sound = $AudioStreamPlayer
 onready var sprite: Sprite = $Sprite
 
 
 func update_stats(stats):
 	if stats[StatManager.Stats.BOTTLES] > 0 && stats[StatManager.Stats.BOTTLES] <= 25:
 		sprite.texture = bottles1
+		bottle_sound.play()
 	elif stats[StatManager.Stats.BOTTLES] > 25 && stats[StatManager.Stats.BOTTLES] <= 50:
 		sprite.texture = bottles2
+		bottle_sound.play()
 	elif stats[StatManager.Stats.BOTTLES] > 50 && stats[StatManager.Stats.BOTTLES] <= 75:
 		sprite.texture = bottles3
+		bottle_sound.play()
 	elif stats[StatManager.Stats.BOTTLES] > 75:
 		sprite.texture = bottles4
+		bottle_sound.play()
 	else:
 		sprite.texture = null
